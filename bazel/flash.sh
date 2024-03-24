84532d7215b0b9bbe20e1a98640e46624388ad9b
Bartosz Śnieg <snieg45@gmail.com>
2024-03-24 12:52:31 +0100

    Basn someip v1 (#9)
    * save
    
    * Test and fix

--- bazel/flash.sh
+++ bazel/flash.sh
@@ -7,4 +7,4 @@ scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $3 "$2:/etc/simb
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $2 "tar -xvf /etc/simba/new/$4-$now.tar -C /"
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $2 " sleep 2 & rm -rf /etc/simba/new/$4-$now.tar"
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $2 " sleep 2 & reboot"
+timeout 5 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=10  $2 " sleep 2 & reboot"
