46057f26968b9f28bcee78ff226d27e18dfab9d7
Bartosz Snieg <snieg45@gmail.com>
2024-04-27 12:09:57 +0200

    Fix and up to v1.5

--- tools/configs/local_app_generator/lib/app.py
+++ tools/configs/local_app_generator/lib/app.py
@@ -8,9 +8,15 @@ class App_someip:
         self.platform = platform
         self.methods  = {}
         for item in methods:
-            self.methods[item["name"]] = item["id"]
+            self.methods[item["name"]] = {}
+            self.methods[item["name"]]["id"] = item["id"]
+            if "access_list" in item:
+                self.methods[item["name"]]["access_list"] = item["access_list"]
+            else:
+                self.methods[item["name"]]["access_list"] = []
         self.events  = {}
         for item in events:
-            self.events[item["name"]] = item["id"]
+            self.events[item["name"]] = {}
+            self.events[item["name"]]["id"] = item["id"]
         self.req_methods = req_methods
         self.req_events = req_events
